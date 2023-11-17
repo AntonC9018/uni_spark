fbb1748b3e0e38d9acaed95381dfb62ba7f05766
AntonC9018 <anton.curm@gmail.com>
2023-11-17 18:45:06 +0200

    Stuff

---
+++ project/scripts/script.ts
@@ -0,0 +1,153 @@
+// Load in the required modules
+import FaceGestures from 'FaceGestures';
+import FaceTracking from 'FaceTracking';
+import Patches from 'Patches';
+import Materials from 'Materials';
+import Textures from 'Textures';
+import Reactive from 'Reactive';
+import { randomInt } from 'crypto';
+
+
+class Emojis<T>
+{
+  smile: T;
+  surprised: T;
+  kiss: T;
+  frown: T;
+  [key: string]: T;
+}
+
+
+(async function() {
+  const face = FaceTracking.face(0);
+
+  const observedStateSignals : Emojis<BoolSignal> = {
+    smile: FaceGestures.isSmiling(face),
+    surprised: FaceGestures.isSurprised(face),
+    kiss: FaceGestures.isKissing(face),
+    frown: FaceGestures.hasEyebrowsFrowned(face),
+  };
+
+  const stateKeys = Object.keys(observedStateSignals);
+
+  let textures: Emojis<TextureBase>;
+  let emojiMaterial: MaterialBase;
+  {
+    const textureKeys = stateKeys;
+    const emojiMaterialKey = "emoji";
+    const promises = [];
+    {
+      const t = Materials.findFirst(emojiMaterialKey);
+      promises.push(t);
+    }
+    for (let key of textureKeys)
+    {
+      const t = Textures.findFirst(key);
+      promises.push(t);
+    }
+    const objects = await Promise.all(promises);
+    emojiMaterial = objects[0];
+
+    {
+      textures = new Emojis<TextureBase>();
+      for (let i = 0; i < textureKeys.length; ++i)
+      {
+        const key = textureKeys[i];
+        textures[key] = objects[i + 1];
+      }
+    }
+  }
+
+  const observedTimeoutMilliseconds : ScalarSignal = 
+    await Patches.outputs.getScalar("observedTimeoutMilliseconds");
+
+  const scoredTimeoutMilliseconds : ScalarSignal =
+    await Patches.outputs.getScalar("scoredTimeoutMilliseconds"); 
+
+
+  const stateSignals = new Emojis<BoolSignalSource>();
+  for (let key of stateKeys)
+  {
+    const signal = Reactive.boolSignalSource(key);
+    stateSignals[key] = signal;
+
+    const sourceSignal = observedStateSignals[key];
+    signal.set(sourceSignal.pinLastValue());
+  }
+
+  // 
+  for (var [key, signal] of Object.entries(observedStateSignals))
+  {
+    const copy = key;
+
+    // No built-in debounce function? wth?
+    let timeout : NodeJS.Timeout = null;
+    signal
+      .monitor()
+      .subscribe(async event => {
+        if (timeout)
+        {
+          clearTimeout(timeout);
+          timeout = null;
+          return;
+        }
+        timeout = setTimeout(() => {
+          stateSignals[copy].set(event.newValue);
+        }, observedTimeoutMilliseconds.pinLastValue());
+      });
+  }
+
+  const currentState = Reactive.stringSignalSource("currentState");
+  {
+    const randomKeyIndex = randomInt(0, stateKeys.length);
+    const randomKey = stateKeys[randomKeyIndex];
+    currentState.set(randomKey);
+  }
+  {
+    let latestSubscription : Subscription = null;
+    let stateSetTimeout : NodeJS.Timeout = null;
+    currentState
+      .signal
+      .monitor({ fireOnInitialValue: true })
+      .subscribe(async event => {
+        const texture = textures[event.newValue];
+        emojiMaterial.setTextureSlot("diffuseTexture", texture.signal);
+
+        if (latestSubscription != null)
+        {
+          latestSubscription.unsubscribe();
+        }
+
+        const signal = stateSignals[event.newValue].signal;
+        latestSubscription = signal
+          .monitor({ fireOnInitialValue: true })
+          .subscribe(event => {
+            if (event.newValue == false)
+            {
+              if (stateSetTimeout != null)
+                clearTimeout(stateSetTimeout);
+            }
+            else
+            {
+              stateSetTimeout = setTimeout(() => {
+                // refresh the current state
+                let currentRandomKey = currentState.signal.pinLastValue();
+                let newRandomKey;
+                do
+                {
+                  const randomKeyIndex = randomInt(0, stateKeys.length);
+                  newRandomKey = stateKeys[randomKeyIndex];
+                }
+                while (currentRandomKey == newRandomKey);
+                currentState.set(newRandomKey);
+
+                stateSetTimeout = null;
+              }, scoredTimeoutMilliseconds.pinLastValue());
+            }
+          });
+      });
+
+  }
+
+  
+})();
---
+++ readme.md
@@ -0,0 +1,24 @@
+Ideea proiectului este să avem un emoji pe ecran, și să repetăm emoția acestuia.
+Pentru aceasta avem nevoie de:
+1. Un set de imagini emoji;
+2. O posibilitate de a le arăta pe ecran;
+3. Posibilitatea de a recunoaște expresia feței utilizatorului;
+4. Să conectăm aceste aspecte.
+
+
+## 1. Set de imagini emoji
+
+Am încercat pur și simplu să adaug un emoji ca text în elementul de text în editor,
+însă acesta aparent nu le suportează.
+Am încercat și diferite fonturi, nu joacă rolul.
+Am căutat ceva referitor la această temă în docuri și pe Google, dar nu am găsit nimic util.
+
+O altă abordare ar fi să folosim niște imagini sau modele 3D din biblioteca lor integrată.
+Acolo nu este nici nu pachet care să includă tot ce am vrut eu, așa că pur și simplu
+am descărcat niște imagini de pe net, primele imagini din Google.
+
+
+## 2. O posibilitate de a le arăta pe ecran
+
+Am menționat că nu-i posibil de făcut asta cu un element text.
+De aceea am 
